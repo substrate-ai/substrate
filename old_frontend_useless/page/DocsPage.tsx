6638c90e088efde12da8ee0a7c0acabfee349276
Baptiste Colle <collebaptiste@gmail.com>
2023-08-02 13:58:31 +0200

    delete old frontend

--- old_frontend_useless/page/DocsPage.tsx
+++
@@ -1,7 +0,0 @@
-export default function PricingPage() {
-    return (
-    <div>
-        <p>Docs</p>
-    </div>
-    )
-    }
--- old_frontend_useless/page/PricingPage.tsx
+++
@@ -1,7 +0,0 @@
-export default function PricingPage() {
-return (
-<div>
-    <p>Pricing</p>
-</div>
-)
-}
