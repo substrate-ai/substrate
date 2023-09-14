44b5ea0effab2fbd100f3a3660bbd1c6b7e92456
kaijenlee <leekaijen@student.tudelft.nl>
2023-09-14 14:24:27 +0200

    fix/refactor(cli): moved registry credential injection to envdclient.py and fix credential setup

--- cli/src/clients/JobClient.py
+++ cli/src/clients/JobClient.py
@@ -4,8 +4,6 @@ from clients.EnvdClient import EnvdClient
 from clients.HttpClient import HttpClient
 from utils.console import console
 from yaspin import yaspin
-import os 
-import json
 
 class JobClient:
     def __init__(self):
@@ -25,74 +23,16 @@ class JobClient:
         # username, password = self.aws_client.get_ecr_login_password()
         # username, password = self.gcp_client.get_docker_credentials()
 
-        self._inject_access_token()
-        self.envd = EnvdClient()
+        self.envd = EnvdClient(self.credentials)
         # self.credentials = self.http_client.get_gcp_credentials()
 
 
         # self.docker_client = DockerClient(self.credentials["registry"], self.credentials["username"], self.credentials["password"], debug=False)
 
-       
-    def _inject_access_token(self): 
-        '''
-        Injects the ECR access token into the client's local ~/.docker/config.json for envd to allow image to be pushed
-        '''
-
-        '''
-        Docker saves authentication settings in the configuration file config.json.
-
-        Linux: ~/.docker/config.json
-        Windows: %USERPROFILE%\.docker\config.json
-        There are separate sections in the file for different authentication methods:
-        
-        source = https://cloud.google.com/artifact-registry/docs/docker/authentication
-        '''
-        # Create the ~/.docker directory if it doesn't exist
-        # TODO check windows support, os.path.expanduser should use the correct path 
-        # src = https://docs.python.org/3/library/os.path.html
-        docker_dir = os.path.expanduser("~/.docker")
-        os.makedirs(docker_dir, exist_ok=True)
-
-        # Create the auth dictionary
-        injected_config = {} 
-
-        ## Modifying Json fields with Python - https://medium.com/@KaranDahiya2000/modify-json-fields-using-python-1b2d88d16908
-        # Write the auth dictionary to ~/.docker/config.json
-        self.docker_config_file = os.path.join(docker_dir, 'config.json')
-        if os.path.isfile(self.docker_config_file) is True: 
-            with open(self.docker_config_file) as existing_config_file:
-                injected_config = json.load(existing_config_file)
-                
-        injected_config["auths"][self.credentials["registry"]]["auth"] = self.credentials['password']
-
-        
-        with open(self.docker_config_file, 'w') as f:
-            json.dump(injected_config, f, indent=4)
-
-        # TODO logs
-        
-
-    def _remove_access_token(self): 
-        '''
-        Remove previous injected Image Registry access token 
-        '''
-        injected_config = {}
-        with open(self.docker_config_file) as existing_config_file:
-            injected_config = json.load(existing_config_file)
-
-        # Remove credentials
-        injected_config["auths"].pop(self.credentials["registry"])
-
-        with open(self.docker_config_file, 'w') as f:
-            json.dump(injected_config, f, indent=4)
-
-
     def start_job(self):
         self.http_client.check_payment_status()
 
         self.envd.build(self.image_name)
-        # TODO check if async is required to ensure that credentials are removed AFTER envd has pushed
-        self._remove_access_token()
 
         # start_build = time.time()
 
